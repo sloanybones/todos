4855e7fbc733ea8922a8faa8adbbcb0149c41a81
Andrew Sloan <sloancodes@gmail.com>
2020-12-03 14:15:20 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+axios
+  .get("https://jsonplaceholder.typicode.com/todos")
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
